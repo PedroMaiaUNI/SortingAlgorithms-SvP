a163d611288371ab4ea1ae18f5122dd6a241d423
UltiMaia <plucasmaiadelima@gmail.com>
2025-10-27 18:17:06 -0300

    add: quicksort (serial)

--- Serial.java
+++ Serial.java
@@ -9,10 +9,9 @@ public class Serial {
     //estrutura basica das funções:
     //  1. monta o novo array que vai ser retornado
     //  2. logica do algoritmo
-    //  3. retorna o nobo array
+    //  3. retorna o novo array
 
     public static int[] BubbleSort(){
-        //classico java: se eu fizesse resultArray = sortArray, ele faria referencia e não uma copia
         int[] resultArray = SortArray.clone();
 
         int swap;
@@ -70,5 +69,39 @@ public class Serial {
         } 
     }
 
-    
+    public static int[] quickSort(){
+        int[] resultArray = SortArray.clone();
+
+        quickS(resultArray, 0, size - 1);
+        
+        return resultArray;
+    }
+
+    private static void quickS(int[] array, int low, int high){
+        if (low<high) {
+            int pi = partitioning(array, low, high);
+            quickS(array, low, pi - 1);
+            quickS(array, pi + 1 ,high);
+        }
+    }
+
+    private static int partitioning(int[] array, int low, int high){
+        int pivot = array[high];
+        int i = low - 1;
+        int swap;
+
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap = array[i];
+                array[i] = array[j];
+                array[j] = swap;
+            }
+        }
+
+        int temp = array[i + 1];
+        array[i+1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
 }
